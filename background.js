8aaf793a82b62a6b1c86552ef1065c3d8a32386f
Jahanvi Mathukia <jahanvipareshkumar.mathukia@ontariotechu.net>
2023-11-05 06:57:57 -0500

    Need to update API KEY

--- background.js
+++ background.js
@@ -1,96 +1,55 @@
-// background.js
 
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//     if (message.text) {
-//       // Send the text to the content script for processing
-//       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//         const activeTab = tabs[0];
-//         chrome.tabs.sendMessage(activeTab.id, { action: 'getResponse' }, function (response) {
-//           // Handle the response from content.js and send it back to the popup
-//           sendResponse(response);
-//         });
-//       });
-//       return true; // Required to indicate an asynchronous response
-//     }
-//   });
+console.log("Background script is running...");
 
-// background.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "getSustainabilityEstimate") {
+    const productDescription = request.productDescription;
+  
+    // Construct the prompt for the ChatGPT API
+    const user_prompt = `${productDescription}.`;
+    console.log(productDescription, user_prompt)
+    // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+    const apiKey = "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";
 
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//     if (message.text) {
-//       // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
-//       const apiKey = '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';
-//       const prompt = message.text;
-  
-//       // Define the API endpoint for ChatGPT
-//       const apiUrl = 'https://api.openai.com/v1/chat/completions';
-  
-//       // Request data
-//       const requestData = {
-//         messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }],
-//         max_tokens: 50, // Adjust as needed
-//       };
-  
-//       // Make a POST request to the ChatGPT API
-//       fetch(apiUrl, {
-//         method: 'POST',
-//         headers: {
-//           'Authorization': `Bearer ${apiKey}`,
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(requestData),
-//       })
-//         .then((response) => response.json())
-//         .then((data) => {
-//           // Send the response to the popup
-//           sendResponse(data.choices[0].message.content);
-//         })
-//         .catch((error) => {
-//           console.error('Error:', error);
-//           sendResponse('Error: Unable to get a response from ChatGPT.');
-//         });
-  
-//       return true; // Required to indicate an asynchronous response
-//     }
-//   });
-  
-  
-// background.js
+    // Define the API endpoint for ChatGPT
+    const apiUrl = "https://api.openai.com/v1/completions";
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.text) {
-      // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
-      const apiKey = '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';
-      const prompt = message.text;
-  
-      // Define the API endpoint for ChatGPT
-      const apiUrl = 'https://api.openai.com/v1/chat/completions';
-  
-      // Request data
-      const requestData = {
-        messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }],
-        max_tokens: 50, // Adjust as needed
-      };
-  
-      // Make a POST request to the ChatGPT API
-      fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${apiKey}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestData),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          sendResponse(data.choices[0].message.content);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-          sendResponse('Error: Unable to get a response from ChatGPT.');
-        });
-  
-      return true; // Required to indicate an asynchronous response
-    }
-  });
-  
+    // Request data
+    const requestData = {
+      "model": "gpt-3.5-turbo-instruct",
+      "prompt": "PLEASE WORK",
+      "max_tokens": 50
+    };
+    
+    // Make a fetch request to the ChatGPT API
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    }).then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    }).then((data) => {
+        console.log(data)
+        if (data.choices && data.choices.length > 0) {
+          const generatedText = data//.choices[0].text; // Extract the text from the first choice
+
+          // Send the response back to the popup script
+          chrome.runtime.sendMessage({ action: "displaySustainabilityEstimate", estimate: generatedText });
+        } else {
+          console.error("Invalid response from ChatGPT:", data);
+          // Send an error message back to the popup script
+          chrome.runtime.sendMessage({ action: "displaySustainabilityEstimate", estimate: "Error: Invalid response from ChatGPT." });
+        }
+    }).catch((error) => {
+        console.error("Error:", error);
+        // Send an error message back to the popup script
+        chrome.runtime.sendMessage({ action: "displaySustainabilityEstimate", estimate: `Error: Unable to get a response from ChatGPT. ${error}` });
+    });
+  }
+});
--- content.js
+++ content.js
@@ -1,13 +1,2 @@
-// content.js
-
-// Handle messages from the background script
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === 'getResponse') {
-      // You can access the active web page content here
-      // For example, you can modify the webpage or retrieve information
-  
-      // Send the response to the background script
-      sendResponse("This is a response from the content script.");
-    }
-  });
-  
+// content.js 
+//extracts, modify and update data from web page
